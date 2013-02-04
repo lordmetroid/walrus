c76d8a82695c2cd680b3ea0a1af1d6268e2ba33d
lordmetroid <lordmetroid@yahoo.com>
2013-02-04 19:31:40 +0100

    Scanning... Error not presents

--- src/symphony_compiler.erl
+++ src/symphony_compiler.erl
@@ -9,23 +9,21 @@
 % @doc
 %% ----------------------------------------------------------------------------
 scan(Template) ->
-	scan(Template, {1,1}, [], text).
+	scan(Template, {1,1}, [{text,{1,1},[]}], text).
 
 scan([], _, Tokens, text) ->
-	%% Finalize last token
-
-	lists:reverse(Tokens);
+	lists:reverse(finalize(Tokens));
 
 
 %% Text
 scan("<%" ++ Rest, {Row,Column}, Tokens, text) ->
 	scan(Rest, {Row,Column+2}, Tokens, start);
 
-scan("\n" ++ Rest, {Row,Column}, Tokens, text) ->
-	scan(Rest, {Row+1,1}, append(Tokens,{Row,Column},"\n"), text);
+scan("\n" ++ Rest, {Row,_Column}, Tokens, text) ->
+	scan(Rest, {Row+1,1}, append("\n",Tokens), text);
 
 scan([Character | Rest], {Row,Column}, Tokens, text) ->
-	scan(Rest, {Row,Column+1}, append(Tokens,{Row,Column},Character), text);
+	scan(Rest, {Row,Column+1}, append(Character,Tokens), text);
 
 
 %% Open tag
@@ -38,11 +36,34 @@ scan("\t" ++ Rest, {Row,Column}, Tokens, start) ->
 scan("\n" ++ Rest, {Row,_Column}, Tokens, start) ->
 	scan(Rest, {Row+1,1}, Tokens, start);
 
+%% Create new variable
 scan([Character | Rest], {Row,Column}, Tokens, start) ->
-	UpdatedTokens =  add({variable,{Row,Column},[Character]}, Tokens),
-	scan(Rest, {Row,Column+1}, UpdatedTokens, variable).
+	UpdatedTokens = add({variable,{Row,Column},[]}, Tokens),
+	scan(Rest, {Row,Column+1}, append(Character,UpdatedTokens), variable);
 
 %% Variable
+
+scan(" " ++ Rest, {Row,Column}, Tokens, variable) ->
+	scan(Rest, {Row,Column+1}, Tokens, stop);
+
+scan("\t" ++ Rest, {Row,Column}, Tokens, variable) ->
+	scan(Rest, {Row,Column+1}, Tokens, stop);
+
+scan("\n" ++ Rest, {Row,_Column}, Tokens, variable) ->
+	scan(Rest, {Row+1,1}, Tokens, stop);
+
+scan("%>" ++ Rest, {Row,Column}, Tokens, variable) ->
+	UpdatedTokens = add({text,{Row,Column+2},[]}, Tokens),
+	scan(Rest, {Row,Column+2}, UpdatedTokens, text);
+
+scan([Character | Rest], {Row,Column}, Tokens, variable) ->
+	scan(Rest, {Row,Column+1}, append(Character,Tokens), variable);
+
+%% Stop tag
+scan("%>" ++ Rest, {Row,Column}, Tokens, stop) ->
+	UpdatedTokens = add({text,{Row,Column+2},[]}, Tokens),
+	scan(Rest, {Row,Column+2}, UpdatedTokens, text).
+	
 
 
 
@@ -50,12 +71,20 @@ scan([Character | Rest], {Row,Column}, Tokens, start) ->
 % @spec
 % @doc
 %% ----------------------------------------------------------------------------
-append(
 add(NewToken, Tokens) ->
-	case Tokens of
-		[] ->
-			[NewToken];
-		[{Type, Position, Content} | Rest] ->
-			[NewToken, {Type,Position,lists:reverse(Content)} | Rest]
-	end.
+	[NewToken | finalize(Tokens)].
+
+%% ----------------------------------------------------------------------------
+% @spec
+% @doc
+%% ----------------------------------------------------------------------------
+append(Character, [{Type,Position,Content} | Rest]) ->
+	[{Type,Position,[Character | Content]} | Rest].
+
+%% ----------------------------------------------------------------------------
+% @spec
+% @doc
+%% ----------------------------------------------------------------------------
+finalize([{Type,Position,Content} | Rest]) ->
+	[{Type,Position,lists:reverse(Content)} | Rest].
 
