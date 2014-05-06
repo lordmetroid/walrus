ee5a76f91fe382f802d723a45b52b5d06d57d8d8
Nicklas W Bjurman <lordmetroid@yahoo.com>
2014-05-06 08:49:54 +0200

    Tidied up scanning routines

--- src/symphony_compiler.erl
+++ src/symphony_compiler.erl
@@ -11,50 +11,33 @@
 scan(Template, Filename) ->
 	scan(Template, Filename,{1,1}, [{text,[]}],[], text).
 
-%% Text
-scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, text) ->
-	scan(Rest, Filename,{Row+1,1}, add("\n",Tokens),Errors, text);
-scan([Character | Rest], Filename,{Row,Column}, Tokens,Errors, text) ->
-	scan(Rest, Filename,{Row,Column+1}, add(Character,Tokens),Errors, text);
-
-%% Start of tag
-scan("<!" ++ Rest, Filename,{Row,Column}, Tokens,Errors, text) ->
-	scan(Rest, Filename,{Row,Column+2}, create(tag,Tokens),Errors, start_tag);
-
-%% Spacers in start of tag
-scan(" " ++ Rest, Filename,{Row,Column}, Tokens,Errors, start_tag) ->
-	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, start_tag);
-scan("\t" ++ Rest, Filename,{Row,Column}, Tokens,Errors, start_tag) ->
-	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, start_tag);
-scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, start_tag) ->
-	scan(Rest, Filename,{Row+1,1}, Tokens,Errors, start_tag);
-
-%% Spacers at end of variable name
-scan(" " ++ Rest, Filename,{Row,Column}, Tokens,Errors, variable) ->
-	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, end_tag);
-scan("\t" ++ Rest, Filename,{Row,Column}, Tokens,Errors, variable) ->
-	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, end_tag);
-scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, variable) ->
-	scan(Rest, Filename,{Row+1,1}, Tokens,Errors, end_tag);
-scan(" " ++ Rest, Filename,{Row,Column}, Tokens,Errors, end_tag) ->
-	scan(Rest, Filename,{Row,Column}, Tokens,Errors, end_tag);
-scan("\t" ++ Rest, Filename,{Row,Column}, Tokens,Errors, end_tag) ->
-	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, end_tag);
-scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, end_tag) ->
-	scan(Rest, Filename,{Row+1,1}, Tokens,Errors, end_tag);
-
-%% End of tag
-scan("!>" ++ Rest, Filename,{Row,Column}, Tokens,Errors, end_tag) ->
-	scan(Rest, Filename,{Row,Column+2}, create(text,Tokens),Errors, text);
-scan("!>" ++ Rest, Filename,{Row,Column}, Tokens,Errors, variable) ->
-	scan(Rest, Filename,{Row,Column+2}, create(text,Tokens),Errors, text);
-
 %% End of file
-scan([], _Filename,{_Row,_Column}, Tokens,_Errors, end_tag) ->
+scan([], _Filename,{_Row,_Column}, Tokens,_Errors, Type) ->
 	%%TODO: print errors?
 	[CurrentToken | Rest] = Tokens,
 	lists:reverse([finalize(CurrentToken) | Rest]).
-	
+
+%% Text special characters
+scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, text) ->
+	scan(Rest, Filename,{Row+1,1}, add("\n",Tokens),Errors, text);
+
+%% Tag special characters
+scan("<!" ++ Rest, Filename,{Row,Column}, Tokens,Errors, text) ->
+	scan(Rest, Filename,{Row,Column+2}, create(tag,Tokens),Errors, tag);
+scan("!>" ++ Rest, Filename,{Row,Column}, Tokens,Errors, tag) ->
+	scan(Rest, Filename,{Row,Column+2}, create(text,Tokens),Errors, text);
+
+scan(" " ++ Rest, Filename,{Row,Column}, Tokens,Errors, tag) ->
+	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, tag);
+scan("\t" ++ Rest, Filename,{Row,Column}, Tokens,Errors, tag) ->
+	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, tag);
+scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, tag) ->
+	scan(Rest, Filename,{Row+1,1}, Tokens,Errors, tag);
+
+%% Add character
+scan([Character | Rest], Filename,{Row,Column}, Tokens,Errors, Type) ->
+	scan(Rest, Filename,{Row,Column+1}, add(Character,Tokens),Errors, Type);
+
 %% ----------------------------------------------------------------------------
 % @spec create(atom, List) -> Tokens
 % @doc Create new token
