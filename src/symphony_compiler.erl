f604c91273c9ffb1db629204262e24d02ed85257
Nicklas W Bjurman <lordmetroid@yahoo.com>
2014-06-18 11:13:52 +0200

    Simplified compiler

--- src/symphony_compiler.erl
+++ src/symphony_compiler.erl
@@ -1,48 +1,48 @@
 -module(symphony_compiler).
 
 -export([
-	scan/2
+	make/1
 ]).
 
 %% ----------------------------------------------------------------------------
-% @spec scan(string(), string()) -> erlsyntax()
+% @spec make(string()) -> erl_syntax()
 % @doc Scan and parse template into erlang syntax form
 %% ----------------------------------------------------------------------------
-scan(Template, Filename) ->
-	scan(Template, Filename,{1,1}, [{text,[]}],[], text).
+make(TemplateString) ->
+	make(TemplateString, {1,1}, [{text,[]}],[], text).
 
 %% End of file
-scan([], _Filename,{_Row,_Column}, Tokens, _Errors, _Type) ->
-	%%TODO: print errors
+make([], {_Row,_Column}, Tokens, _Errors, _Type) ->
+	%% TODO: print errors
 	[CurrentToken | Rest] = Tokens,
 	erl_syntax:list([finalize(CurrentToken) | Rest]);
 
 %% Text special characters
-scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, text) ->
-	scan(Rest, Filename,{Row+1,1}, add("\n",Tokens),Errors, text);
+make("\n" ++ Rest, {Row,_Column}, Tokens,Errors, text) ->
+	make(Rest, {Row+1,1}, add("\n",Tokens),Errors, text);
 
 %% Tag special characters
-%% TODO: Find misplaced start and end tag token errors
-scan("<!" ++ Rest, Filename,{Row,Column}, Tokens,Errors, text) ->
-	scan(Rest, Filename,{Row,Column+2}, create(tag,Tokens),Errors, tag);
-scan("!>" ++ Rest, Filename,{Row,Column}, Tokens,Errors, tag) ->
-	scan(Rest, Filename,{Row,Column+2}, create(text,Tokens),Errors, text);
+%% TODO: Find misplaced start and end variable token errors
+make("<?" ++ Rest, {Row,Column}, Tokens,Errors, text) ->
+	make(Rest, {Row,Column+2}, create(variable,Tokens),Errors, variable);
+make("?>" ++ Rest, Row,Column}, Tokens,Errors, variable) ->
+	make(Rest, {Row,Column+2}, create(text,Tokens),Errors, text);
 
-scan(" " ++ Rest, Filename,{Row,Column}, Tokens,Errors, tag) ->
-	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, tag);
-scan("\t" ++ Rest, Filename,{Row,Column}, Tokens,Errors, tag) ->
-	scan(Rest, Filename,{Row,Column+1}, Tokens,Errors, tag);
-scan("\n" ++ Rest, Filename,{Row,_Column}, Tokens,Errors, tag) ->
-	scan(Rest, Filename,{Row+1,1}, Tokens,Errors, tag);
+make(" " ++ Rest, {Row,Column}, Tokens,Errors, variable) ->
+	make(Rest, {Row,Column+1}, Tokens,Errors, variable);
+make("\t" ++ Rest, {Row,Column}, Tokens,Errors, variable) ->
+	make(Rest, {Row,Column+1}, Tokens,Errors, variable);
+make("\n" ++ Rest, {Row,_Column}, Tokens,Errors, variable) ->
+	make(Rest, {Row+1,1}, Tokens,Errors, variable);
 
 %% Add character
-scan([Character | Rest], Filename,{Row,Column}, Tokens,Errors, Type) ->
-	%% TODO: Allow only alphanumerical in tags
-	%% TODO: Find tag-name errors
-	scan(Rest, Filename,{Row,Column+1}, add(Character,Tokens),Errors, Type).
+make([Character | Rest], {Row,Column}, Tokens,Errors, Type) ->
+	%% TODO: Allow only alphanumerical in variables
+	%% TODO: Find variable-name errors
+	make(Rest, {Row,Column+1}, add(Character,Tokens),Errors, Type).
 
 %% ----------------------------------------------------------------------------
-% @spec create(atom, List) -> Tokens
+% @spec create(atom, list()) -> Tokens
 % @doc Create new token
 %% ----------------------------------------------------------------------------
 create(Type, Tokens) ->
@@ -50,7 +50,7 @@ create(Type, Tokens) ->
 	[{Type,[]}, finalize(Token) | Rest].
 
 %% ----------------------------------------------------------------------------
-% @spec add(List::string(), List) -> Tokens
+% @spec add(character(), list()) -> Tokens::list()
 % @doc Add new character to token
 %% ----------------------------------------------------------------------------
 add(Character, Tokens) ->
@@ -66,4 +66,3 @@ finalize({Type, String}) ->
 		erl_syntax:atom(Type),
 		erl_syntax:string(lists:reverse(String))
 	]).
-
